58ac14cb7b014df5c7fc04632282bb3f6aa195c7
Oliverlyc <82738320@qq.com>
2018-12-21 18:22:55 +0800

    fix

--- app/Http/Controllers/ProjectCostController.php
+++ app/Http/Controllers/ProjectCostController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
-use Excel;
-use Collection;
 class ProjectCostController extends Controller
 {
     public $type = [
